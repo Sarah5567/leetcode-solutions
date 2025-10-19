2bb69f23e8a6a6c5e8a03b10029b5d9eb33609e9
Sarah Gershuni <Sarah556726@gmail.com>
2025-10-19 10:29:48 +0300

    Time: 41 ms (13.96%), Space: 19.3 MB (54.03%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,28 +1,36 @@
 class Solution:
-    def findOneNumber(self, nums: List[int], bitNum: int, identifier: int):
-        bitAt = [0, 0]
-        mask, matchBits, i = 0, 0, 0
-        
-        while not (bitAt[0] == 1 or bitAt[1] == 1):
-            bitAt = [0, 0]
-            for num in nums:
-                if ((num >> bitNum) & 1) == identifier and (num & mask) == matchBits:
-                    bitAt[(num >> i) & 1] += 1
-
-            if bitAt[1] % 2 == 1:
-                matchBits |= 1 << i
-            mask *= 2
-            mask += 1
-            i += 1
-
-        return next(
-            (num for num in nums if ((num >> bitNum) & 1) == identifier and (num & mask) == matchBits),
-            None
-        )
-
     def singleNumber(self, nums: List[int]) -> List[int]:
         bitNum = 0
         while sum((num >> bitNum) & 1 for num in nums) % 2 == 0:
             bitNum += 1
 
-        return [self.findOneNumber(nums, bitNum, 0), self.findOneNumber(nums, bitNum, 1)]
+        bitAt = [[0, 0], [0, 0]]
+        mask, matchBits0, matchBits1, i = 0, 0, 0, 0
+        
+        while not (bitAt[0][0] == 1 or bitAt[0][1] == 1) or not (bitAt[1][0] == 1 or bitAt[1][1] == 1):
+            bitAt = [[0, 0], [0, 0]]
+            for num in nums:
+                if ((num >> bitNum) & 1) == 0:
+                    if (num & mask) == matchBits0:
+                        bitAt[0][(num >> i) & 1] += 1
+                elif (num & mask) == matchBits1:
+                    bitAt[1][(num >> i) & 1] += 1
+
+            if bitAt[0][1] % 2 == 1:
+                matchBits0 |= 1 << i
+            if bitAt[1][1] % 2 == 1:
+                matchBits1 |= 1 << i
+
+            mask *= 2
+            mask += 1
+            i += 1
+
+        first = next(
+            (num for num in nums if ((num >> bitNum) & 1) == 0 and (num & mask) == matchBits0),
+            None
+        )
+        second = next(
+            (num for num in nums if ((num >> bitNum) & 1) == 1 and (num & mask) == matchBits1),
+            None
+        )
+        return [first, second]
