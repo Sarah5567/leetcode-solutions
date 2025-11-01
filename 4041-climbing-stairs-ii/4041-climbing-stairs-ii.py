00f15e675abf337a5d19b8c999d0ae9b5a20ce89
Sarah Gershuni <Sarah556726@gmail.com>
2025-11-01 18:30:06 +0200

    Time: 1031 ms (26.54%), Space: 29.5 MB (47.39%) - LeetHub

--- 4041-climbing-stairs-ii/4041-climbing-stairs-ii.py
+++ 4041-climbing-stairs-ii/4041-climbing-stairs-ii.py
@@ -4,9 +4,9 @@ class Solution:
         dp : List[int] = [float('inf') for _ in range(dpLen)]
         dp[0] = 0
 
-        for i in range(0, dpLen - 1):
-            for j in range(i + 1, i + 4):
-                if j < dpLen:
+        for j in range(0, dpLen):
+            for i in range(j - 3, j):
+                if i >= 0:
                     dp[j] = min(dp[j], dp[i] + costs[j - 1] + (j - i) ** 2)
 
         return dp[dpLen - 1]
