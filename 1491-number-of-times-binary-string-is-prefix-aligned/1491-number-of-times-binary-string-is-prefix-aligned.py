c85a53930ac7b3b8188980b9579e1910a4616644
Sarah Gershuni <Sarah556726@gmail.com>
2025-11-01 18:39:49 +0200

    Time: 34 ms (51.06%), Space: 23.5 MB (21.28%) - LeetHub

--- 1491-number-of-times-binary-string-is-prefix-aligned/1491-number-of-times-binary-string-is-prefix-aligned.py
+++ 1491-number-of-times-binary-string-is-prefix-aligned/1491-number-of-times-binary-string-is-prefix-aligned.py
@@ -2,8 +2,8 @@ class Solution:
     def numTimesAllBlue(self, flips: List[int]) -> int:
         maxFlip, count = 0, 0
 
-        for i, flip in enumerate(flips):
-            maxFlip = max(flip, maxFlip)
+        for i in range(len(flips)):
+            maxFlip = max(flips[i], maxFlip)
             if i == maxFlip - 1:
                 count += 1
 
