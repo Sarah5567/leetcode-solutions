5d5983abb746534f7bd02cd0b81eae15d2779bf6
Sarah Gershuni <Sarah556726@gmail.com>
2025-11-08 20:24:45 +0200

    Time: 6 ms (66.39%), Space: 36.4 MB (98.74%) - LeetHub

--- 2669-find-the-substring-with-maximum-cost/2669-find-the-substring-with-maximum-cost.cpp
+++ 2669-find-the-substring-with-maximum-cost/2669-find-the-substring-with-maximum-cost.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    int maximumCostSubstring(string s, string chars, vector<int>& vals) {
-        const int ALPHABET_SIZE = 26;
-        vector<int> cost(ALPHABET_SIZE);
-        iota(cost.begin(), cost.end(), 1);
+    int maximumCostSubstring(const string& s, const string& chars, const vector<int>& vals) {
+        static constexpr int ALPHABET_SIZE = 26;
+        int cost[ALPHABET_SIZE];
+        for (int i = 0; i < ALPHABET_SIZE; ++i)
+            cost[i] = i + 1;
 
-        for(int i = 0; i < chars.size(); i++){
+        for (int i = 0; i < (int)chars.size(); ++i)
             cost[chars[i] - 'a'] = vals[i];
-        }
 
         int maxSum = 0;
         int curSum = 0;
         int minSum = 0;
-        int minIndex = -1;
-        for(int i = 0; i < s.length(); i++){
-            curSum += cost[s[i] - 'a'];
-            if(curSum < minSum){
+
+        for (char c : s) {
+            curSum += cost[c - 'a'];
+            if (curSum < minSum)
                 minSum = curSum;
-                minIndex = i;
-            }
-            else if(maxSum < curSum - minSum){
+            else if (maxSum < curSum - minSum)
                 maxSum = curSum - minSum;
-            }
         }
 
-        return maxSum;       
+        return maxSum;
     }
 };
