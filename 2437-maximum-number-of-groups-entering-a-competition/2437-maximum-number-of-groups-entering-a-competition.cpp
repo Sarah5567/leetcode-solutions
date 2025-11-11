e9bb712fd15b29b77664907d5813bbd3408fd5a3
Sarah Gershuni <Sarah556726@gmail.com>
2025-11-11 19:06:53 +0200

    Time: 0 ms (100%), Space: 60.3 MB (28.8%) - LeetHub

--- 2437-maximum-number-of-groups-entering-a-competition/2437-maximum-number-of-groups-entering-a-competition.cpp
+++ 2437-maximum-number-of-groups-entering-a-competition/2437-maximum-number-of-groups-entering-a-competition.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int maximumGroups(vector<int>& grades) {
-        return floor((-1 + sqrt(1 + 8 * grades.size())) / 2);
-
+        return (-1 + sqrt(1 + 8 * grades.size())) / 2;
     }
 };
