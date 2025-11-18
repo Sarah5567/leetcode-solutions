63bf4a3443e600ea776c9d509e64fcc9ef468832
Sarah Gershuni <Sarah556726@gmail.com>
2025-11-18 22:37:34 +0200

    Time: 11 ms (44.42%), Space: 18.8 MB (34.61%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -2,10 +2,10 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         jumps = 0
         curBorder, nextBorder = 1, 1
-        for i, num in enumerate(nums):
+        for i in range(len(nums)):
             if i == curBorder:
                 jumps += 1
                 curBorder = nextBorder
-            nextBorder = max(nextBorder, i + num + 1)
+            nextBorder = max(nextBorder, i + nums[i] + 1)
 
         return jumps
