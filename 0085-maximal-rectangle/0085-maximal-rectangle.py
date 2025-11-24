a7871d079ae45cf9977367b7f4427cf3458cdda2
Sarah Gershuni <Sarah556726@gmail.com>
2025-11-24 11:56:59 +0200

    Time: 51 ms (28.49%), Space: 25.1 MB (6.63%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -3,26 +3,34 @@ class Solution:
         if not matrix or not matrix[0]:
             return 0
 
-        m, n = len(matrix), len(matrix[0])
+        m = len(matrix)
+        n = len(matrix[0])
 
+        # local bindings for speed
         width = [[0] * n for _ in range(m)]
+        matrix0 = matrix[0]
 
         # compute consecutive widths
         for i in range(m):
-            width[i][0] = int(matrix[i][0])
+            row = matrix[i]
+            w = width[i]
+            w[0] = 1 if row[0] == '1' else 0
             for j in range(1, n):
-                width[i][j] = width[i][j - 1] + 1  if matrix[i][j] == '1' else 0
+                if row[j] == '1':
+                    w[j] = w[j - 1] + 1
+                else:
+                    w[j] = 0
 
         # compute up-span
         up = [[0] * n for _ in range(m)]
         for j in range(n):
             stack = []
             for i in range(m):
-                while stack and width[stack[-1]][j] > width[i][j]:
+                cur_width = width[i][j]
+                while stack and width[stack[-1]][j] > cur_width:
                     stack.pop()
 
                 up[i][j] = i + 1 if not stack else i - stack[-1]
-
                 stack.append(i)
 
         # compute down-span
@@ -30,18 +38,23 @@ class Solution:
         for j in range(n):
             stack = []
             for i in range(m - 1, -1, -1):
-                while stack and width[stack[-1]][j] >= width[i][j]:
+                cur_width = width[i][j]
+                while stack and width[stack[-1]][j] >= cur_width:
                     stack.pop()
 
                 down[i][j] = (m - i) if not stack else (stack[-1] - i)
-
                 stack.append(i)
 
         # compute maximal rectangle
         maxRect = 0
         for i in range(m):
+            w_row = width[i]
+            up_row = up[i]
+            down_row = down[i]
             for j in range(n):
-                height = up[i][j] + down[i][j] - 1
-                maxRect = max(maxRect, width[i][j] * height)
+                h = up_row[j] + down_row[j] - 1
+                area = w_row[j] * h
+                if area > maxRect:
+                    maxRect = area
 
         return maxRect
