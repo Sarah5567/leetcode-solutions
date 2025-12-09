99c5c622f302cac93dae871a818177af9f60ad07
Sarah Gershuni <Sarah556726@gmail.com>
2025-12-09 10:14:52 +0200

    Time: 12 ms (77.94%), Space: 23.8 MB (92.2%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int maxDistinct(string s) {
-        vector<bool> letters(26, false);
+        constexpr int ALPHABET_SIZE = 26;
+        vector<bool> letters(ALPHABET_SIZE, false);
+
         int count = 0;
         for(auto& ch : s){
-            if(!letters[ch - 'a'])
+            if(!letters[ch - 'a']){
                 count++;
-            letters[ch - 'a'] = true;
+                letters[ch - 'a'] = true;
+            }
         }
         return count;
 
