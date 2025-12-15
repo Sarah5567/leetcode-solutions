0dceac8d4c2e07c34c8378e48a363e9777e4527e
Sarah Gershuni <Sarah556726@gmail.com>
2025-12-15 22:10:47 +0200

    Time: 137 ms (97.82%), Space: 27 MB (68.12%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -2,20 +2,9 @@ class Solution:
     def maxScoreIndices(self, nums: List[int]) -> List[int]:
         n = len(nums)
         division_score = sum(nums)
+        res = [0]
 
         highest_score = division_score
-        for num in nums:
-            if num:
-                division_score -= 1
-            else:
-                division_score += 1
-                highest_score = max(highest_score, division_score)
-        
-        res = []
-
-        division_score = sum(nums)
-        if division_score == highest_score:
-            res.append(0)
         for i, num in enumerate(nums):
             if num:
                 division_score -= 1
@@ -23,6 +12,9 @@ class Solution:
                 division_score += 1
                 if division_score == highest_score:
                     res.append(i + 1)
+                elif division_score > highest_score:
+                    highest_score = division_score
+                    res = [i + 1]
 
         return res
 
