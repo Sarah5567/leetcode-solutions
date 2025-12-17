1f60e9cf79bf7dbd32b079ccd8cae64aa84bacf7
Sarah Gershuni <Sarah556726@gmail.com>
2025-12-17 15:13:13 +0200

    Time: 11 ms (89.17%), Space: 19.5 MB (22.03%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -2,14 +2,18 @@ class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         n : int = len(customers)
         technique_gain : int = sum(customers[i] * grumpy[i] for i in range(minutes))
-        satisfied_customers : int = technique_gain
+        satisfied_customers : int = sum(customers[i] for i in range(minutes) if not grumpy[i])
+        max_technique_gain : int = technique_gain
 
         for i in range(minutes, n):
             leaving_customers = i - minutes
             technique_gain -= customers[leaving_customers] * grumpy[leaving_customers]
-            technique_gain += customers[i] * grumpy[i]
-            satisfied_customers = max(satisfied_customers, technique_gain)
 
-        satisfied_customers += sum(customers[i] for i in range(n) if not grumpy[i])
-        return satisfied_customers
+            if grumpy[i]:
+                technique_gain += customers[i]
+                max_technique_gain = max(max_technique_gain, technique_gain)
+            else:
+                satisfied_customers += customers[i]
+
+        return satisfied_customers + max_technique_gain
        
