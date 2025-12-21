6f6fb48367fb08969402a1f8e34b091daa7da9b4
Sarah Gershuni <Sarah556726@gmail.com>
2025-12-21 15:26:34 +0200

    Time: 607 ms (97.8%), Space: 38.7 MB (81.32%) - LeetHub

--- 3598-longest-common-prefix-between-adjacent-strings-after-removals/3598-longest-common-prefix-between-adjacent-strings-after-removals.py
+++ 3598-longest-common-prefix-between-adjacent-strings-after-removals/3598-longest-common-prefix-between-adjacent-strings-after-removals.py
@@ -1,13 +1,12 @@
 class Solution:
-    def prefix(self, s1 : str, s2 : str, begin : int = 0) -> int:
-        length = begin
+    def prefix(self, s1: str, s2: str, begin: int = 0) -> int:
+        limit = min(len(s1), len(s2))
+        i = begin
 
-        for i1, i2 in zip(range(begin, len(s1)), range(begin, len(s2))):
-            if s1[i1] != s2[i2]:
-                break
-            length += 1
+        while i < limit and s1[i] == s2[i]:
+            i += 1
 
-        return length
+        return i
 
     def longestCommonPrefix(self, words: List[str]) -> List[int]:
         n = len(words)
@@ -18,27 +17,21 @@ class Solution:
         prev = -1
 
         for i in range(1, n):
-
             length = self.prefix(words[i - 1], words[i])
 
             if length > max_lcp:
-                second_max_lcp = max_lcp
-                max_lcp = length
+                second_max_lcp, max_lcp = max_lcp, length
                 max_lcp_index = i
             else:
                 second_max_lcp = max(second_max_lcp, length)
 
             if prev == length:
-                res[i - 1] = self.prefix(words[i - 2], words[i], begin = length)
+                res[i - 1] = self.prefix(words[i - 2], words[i], begin=length)
 
             prev = length
 
-        max_lcp_indexes = {max_lcp_index, max_lcp_index - 1}
-
         for i in range(n):
-            res[i] = max(
-                res[i],
-                second_max_lcp if i in max_lcp_indexes else max_lcp
-            )
+            candidate = second_max_lcp if i == max_lcp_index or i == max_lcp_index - 1 else max_lcp
+            res[i] = max(res[i], candidate)
 
         return res
