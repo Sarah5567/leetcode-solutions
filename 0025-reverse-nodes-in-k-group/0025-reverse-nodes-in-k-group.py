03394854f3c5e5261e875ac027e188eea9eaaa27
Sarah Gershuni <Sarah556726@gmail.com>
2025-12-23 16:05:58 +0200

    Time: 6 ms (6.28%), Space: 18.1 MB (97.35%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -7,34 +7,27 @@ class Solution:
     def reverse_one_group(self, node : Optional[ListNode], k : int, i : int) -> Tuple[ListNode, ListNode]:
         if i == k:
             return (node, node.next)
+        if not node.next:
+            return (None, None)
         else:
             first, last = self.reverse_one_group(node.next, k, i + 1)
-            node.next.next = node
+            if first:
+                node.next.next = node
             return first, last
 
-    def has_k_nodes(self, node : ListNode, k : int) -> bool:
-        for _ in range(k):
-            if not node:
-                return False
-            node = node.next
-        return True
-
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if not self.has_k_nodes(head, k):
-            return head
-
         first, last = self.reverse_one_group(head, k, 1)
         head.next = last
         prev = head
         head = first
 
-        while self.has_k_nodes(last, k):
+        while last:
             first, last = self.reverse_one_group(last, k, 1)
-            prev.next.next = last
+            if first:
+                prev.next.next = last
 
-            cur_tail = prev.next
-            prev.next = first
-            prev = cur_tail
-
+                cur_tail = prev.next
+                prev.next = first
+                prev = cur_tail
 
         return head
