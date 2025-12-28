7832cac639ec17cbd3f504537f68649c457456ee
Sarah Gershuni <Sarah556726@gmail.com>
2025-12-28 14:44:15 +0200

    Time: 87 ms (83.94%), Space: 24.2 MB (41.61%) - LeetHub

--- 2227-encrypt-and-decrypt-strings/2227-encrypt-and-decrypt-strings.py
+++ 2227-encrypt-and-decrypt-strings/2227-encrypt-and-decrypt-strings.py
@@ -2,27 +2,19 @@ class Encrypter:
 
     def __init__(self, keys, values, dictionary):
         self.keys_map = {k: v for k, v in zip(keys, values)}
-        self.dictionary = dictionary
-        self.encrypt_cache = {}
+        self.encrypted_dictionary = defaultdict(int)
+
+        for word in dictionary:
+            encrypted = self.encrypt(word)
+            self.encrypted_dictionary[encrypted] += 1
 
     def encrypt(self, word1: str) -> str:
-        if word1 in self.encrypt_cache:
-            return self.encrypt_cache[word1]
-
         res = []
         for ch in word1:
             if ch not in self.keys_map:
                 return ""
             res.append(self.keys_map[ch])
-        joined_res = "".join(res)
-
-        if word1 in self.encrypt_cache:
-            self.encrypt_cache[word1] = joined_res
-        return joined_res
+        return "".join(res)
 
     def decrypt(self, word2: str) -> int:
-        count = 0
-        for word in self.dictionary:
-            if self.encrypt(word) == word2:
-                count += 1
-        return count
+        return self.encrypted_dictionary[word2]
