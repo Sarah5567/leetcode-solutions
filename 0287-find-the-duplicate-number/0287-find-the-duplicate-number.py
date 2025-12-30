581c673eb078bf84bbecc6123c0c1b25b86a9c65
Sarah Gershuni <Sarah556726@gmail.com>
2025-12-30 14:16:17 +0200

    Time: 601 ms (5.09%), Space: 31.4 MB (30.44%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def count_ones_upto(self, n, bit):
+        cycle = 1 << (bit + 1)
+        full_cycles = (n + 1) // cycle
+        remainder = (n + 1) % cycle
+        return full_cycles * (cycle >> 1) + max(0, remainder - (cycle >> 1))
+
+    def findDuplicate(self, nums: List[int]) -> int:
+        n = len(nums) - 1
+        duplicate  = 0
+        cur_pow = 2
+
+        for bit in range(32):
+            ones = 0
+            for num in nums:
+                ones += (num >> bit) & 1
+
+            expected_ones = self.count_ones_upto(n, bit)
+
+            if ones > expected_ones:
+                duplicate  |= (1 << bit)
+            cur_pow *= 2
+        
+        return duplicate
