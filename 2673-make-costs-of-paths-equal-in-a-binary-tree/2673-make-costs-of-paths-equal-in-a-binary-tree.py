886f45116c1295075a73e97ddb63fbf7ed8d1827
Sarah Gershuni <Sarah556726@gmail.com>
2026-01-01 23:02:46 +0200

    Time: 245 ms (52.76%), Space: 26.6 MB (63.8%) - LeetHub

--- 2673-make-costs-of-paths-equal-in-a-binary-tree/2673-make-costs-of-paths-equal-in-a-binary-tree.py
+++ 2673-make-costs-of-paths-equal-in-a-binary-tree/2673-make-costs-of-paths-equal-in-a-binary-tree.py
@@ -1,29 +1,12 @@
 class Solution:
     def minIncrements(self, n: int, cost: List[int]) -> int:
         max_from_leaf = [0] * (n + 1)
+        res = 0
         for i in range(n, 0, -1):
-            cur = max_from_leaf[i] + cost[i - 1]
-            parent = i // 2
-            max_from_leaf[i] = cur
-            if parent > 0:
-                max_from_leaf[parent] = max(max_from_leaf[parent], cur)
+            max_from_leaf[i] += cost[i - 1]
+            max_from_leaf[i//2] = max(max_from_leaf[i//2], max_from_leaf[i])
+            if i * 2 + 1 <= n:
+                res += abs(max_from_leaf[i * 2] - max_from_leaf[i * 2 + 1])
 
-        target = max_from_leaf[1]
-
-        max_from_root = [0] * (n + 1)
-
-        def min_increment(i: int) -> int:
-            if i > n:
-                return 0
-
-            parent = i // 2
-            left = i * 2
-            right = left + 1
-
-            max_from_root[i] = max_from_root[parent] + cost[i - 1]
-            inc = target - max_from_root[parent] - max_from_leaf[i]
-            max_from_root[i] += inc
-
-            return inc + min_increment(left) + min_increment(right)
-
-        return min_increment(1)
+        return res
+        
