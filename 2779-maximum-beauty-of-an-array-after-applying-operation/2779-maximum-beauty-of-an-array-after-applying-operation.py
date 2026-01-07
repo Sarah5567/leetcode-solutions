873c32e2ebf524cf9bfd2e7b646d16e84b05f5e8
Sarah Gershuni <Sarah556726@gmail.com>
2026-01-07 19:55:22 +0200

    Time: 283 ms (22.45%), Space: 32.5 MB (18.6%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,9 +1,9 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
         nums.sort()
-        max_beauty = 0
+        max_beauty, begin_idx = 0, 0
         for idx, num in enumerate(nums):
-            begin_idx = bisect_left(nums, num - k * 2)
+            begin_idx = bisect_left(nums, num - k * 2, lo=begin_idx, hi=idx)
             max_beauty = max(max_beauty, idx - begin_idx + 1)
 
         return max_beauty
