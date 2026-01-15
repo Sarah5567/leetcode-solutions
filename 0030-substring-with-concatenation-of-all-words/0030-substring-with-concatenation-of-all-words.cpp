5c8f906245977a3e942de608eebe2652180a8373
Sarah Gershuni <Sarah556726@gmail.com>
2026-01-15 13:54:10 +0200

    Time: 3 ms (99.6%), Space: 20.3 MB (98.3%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-            int n = words.size(), len = words[0].size();
-            vector<int> res;
+        int n = words.size(), len = words[0].size();
+        vector<int> res;
 
-            unordered_map<string,int> freq;
-            for (auto& word : words) freq[word]++;
-  
-            for(int startPoint = 0; startPoint < len; startPoint++){
-                int curN = 0;
-                unordered_map<string,int> curFreq;
-                for (const auto& [word, frequency] : freq) curFreq[word] = 0;
+        unordered_map<string_view, int> freq;
+        freq.reserve(words.size());
+        for (auto& w : words) freq[string_view(w)]++;
 
-                for(int i = startPoint; i + len <= s.size(); i += len){
-                    if(i - n * len >= 0){
-                        string removedWord = s.substr(i - n * len, len);
+        for (int startPoint = 0; startPoint < len; startPoint++) {
+            int curN = 0;
+            unordered_map<string_view, int> curFreq;
+            curFreq.reserve(freq.size());
 
-                        if(curFreq.find(removedWord) != curFreq.end()){
-                            if(curFreq[removedWord] <= freq[removedWord])
-                                curN -= 1;
-                            curFreq[removedWord] -= 1;
-                        }
+            for (int i = startPoint; i + len <= (int)s.size(); i += len) {
+
+                if (i - n * len >= 0) {
+                    string_view removedWord(s.data() + (i - n * len), len);
+                    auto itRem = freq.find(removedWord);
+                    if (itRem != freq.end()) {
+                        int &cnt = curFreq[removedWord];
+                        if (cnt <= itRem->second) curN--;
+                        cnt--;
                     }
-
-                    string curWord = s.substr(i, len);
-                    if(curFreq.find(curWord) != curFreq.end()){
-                        curFreq[curWord] += 1;
-
-                        if(curFreq[curWord] <= freq[curWord])
-                            curN += 1;
-                    }
-
-                    if(curN == n) res.push_back(i - (n - 1) * len);
                 }
 
+                string_view curWord(s.data() + i, len);
+                auto it = freq.find(curWord);
+                if (it != freq.end()) {
+                    int &cnt = curFreq[curWord];
+                    cnt++;
+                    if (cnt <= it->second) curN++;
+                }
+
+                if (curN == n) res.push_back(i - (n - 1) * len);
             }
-            return res;
+        }
+
+        return res;
     }
 };
