e4ed016e326502aa3888f68df0dcc74ac276bb0a
Sarah Gershuni <Sarah556726@gmail.com>
2026-01-18 01:11:54 +0200

    Time: 180 ms (30.34%), Space: 157.2 MB (53.21%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
         set<long long> s;
-        for(int i = 0; i < nums.size(); i++){
-            if(i > indexDiff)
+
+        for (int i = 0, n = nums.size(); i < n; i++) {
+            if (i > indexDiff)
                 s.erase(nums[i - indexDiff - 1]);
 
             long long curVal = nums[i];
@@ -12,7 +13,7 @@ public:
             if (it != s.end() && *it <= curVal + valueDiff)
                 return true;
 
-            s.insert(nums[i]);
+            s.insert(curVal);
         }
         return false;
     }
