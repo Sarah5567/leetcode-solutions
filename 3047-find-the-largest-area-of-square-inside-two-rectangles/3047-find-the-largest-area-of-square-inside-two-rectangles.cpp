11cf317bf5373bb57c4729df466354a48168915e
Sarah Gershuni <Sarah556726@gmail.com>
2026-01-17 20:34:49 +0200

    Time: 113 ms (41.32%), Space: 59.3 MB (80.17%) - LeetHub

--- 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.cpp
+++ 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    bool overlap(vector<int>& bottomLeft1, vector<int>& topRight1, vector<int>& bottomLeft2, vector<int>& topRight2){
-        if(min(topRight1[0], topRight2[0]) <= max(bottomLeft1[0], bottomLeft2[0]))
-            return false;
+    long long largestSquareArea(vector<vector<int>>& bottomLeft,
+                                vector<vector<int>>& topRight) {
+        int n = bottomLeft.size();
+        long long longestSide = 0;
 
-        return min(topRight1[1], topRight2[1]) > max(bottomLeft1[1], bottomLeft2[1]);
-    }
-    long long largestSquareArea(vector<vector<int>>& bottomLeft, vector<vector<int>>& topRight) {
-        int longestSide = 0, n = bottomLeft.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int height = min(topRight[i][1], topRight[j][1]) -
+                             max(bottomLeft[i][1], bottomLeft[j][1]);
+                int width  = min(topRight[i][0], topRight[j][0]) -
+                             max(bottomLeft[i][0], bottomLeft[j][0]);
 
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(overlap(bottomLeft[i], topRight[i], bottomLeft[j], topRight[j])){
-                    int height = min(topRight[i][1], topRight[j][1]) - max(bottomLeft[i][1], bottomLeft[j][1]);
-                    int width = min(topRight[i][0], topRight[j][0]) - max(bottomLeft[i][0], bottomLeft[j][0]);
-                    longestSide = max(longestSide, min(height, width));
+                if (height > 0 && width > 0) {
+                    longestSide = max(longestSide,
+                                      (long long)min(height, width));
                 }
             }
         }
 
-        return 1LL * longestSide * longestSide;
+        return longestSide * longestSide;
     }
 };
