3982b069f5bf164af91c3bf05255e68405e84a93
Sarah Gershuni <Sarah556726@gmail.com>
2026-01-31 18:48:01 +0200

    Time: 0 ms (100%), Space: 22.6 MB (46.36%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int n = citations.size();
-        int right = min(n, citations[n - 1]), left = 1;
+        int n = (int)citations.size();
+        if (n == 0) return 0;
 
-        while(left <= right){
+        if (citations[n - 1] == 0) return 0;
+        if (citations[0] >= n) return n;
+
+        int left = 0, right = n;
+
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            
-            if(citations[n - mid] >= mid)
-                left = mid + 1;      
-            else
-                right = mid - 1;
-        }
 
+            if (mid == 0 || citations[n - mid] >= mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
         return right;
     }
 };
