b89bc2c457098e3473c0d5337e2c6bcab91d14ef
Sarah Gershuni <Sarah556726@gmail.com>
2026-02-02 23:19:27 +0200

    Time: 4 ms (72.96%), Space: 18.9 MB (61.99%) - LeetHub

---
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.cpp
@@ -0,0 +1,41 @@
+class NestedIterator {
+private:
+    stack<pair<const vector<NestedInteger>*, int>> st;
+
+    void moveToNextInteger() {
+        while (!st.empty()) {
+            auto &[lst, idx] = st.top();
+
+            if (idx >= (int)lst->size()) {
+                st.pop();
+                if (!st.empty()) st.top().second++;
+                continue;
+            }
+
+            if ((*lst)[idx].isInteger()) return;
+
+            const vector<NestedInteger> &sub = (*lst)[idx].getList();
+            st.push({&sub, 0});
+        }
+    }
+
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        st.push({&nestedList, 0});
+        moveToNextInteger();
+    }
+
+    int next() {
+        int val = (*st.top().first)[st.top().second].getInteger();
+        st.top().second++;
+
+        moveToNextInteger();
+
+        return val;
+    }
+
+    bool hasNext() {
+        moveToNextInteger();
+        return !st.empty();
+    }
+};
