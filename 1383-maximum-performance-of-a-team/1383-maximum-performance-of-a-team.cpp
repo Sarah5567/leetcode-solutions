de599071d104c290f4816ff72c530e4c6c25567d
Sarah Gershuni <Sarah556726@gmail.com>
2026-02-03 14:27:19 +0200

    Time: 23 ms (47.88%), Space: 39 MB (99.5%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,37 +1,43 @@
 class Solution {
 public:
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
-        const int MOD = 1'000'000'007;
+        static constexpr int MOD = 1'000'000'007;
 
         vector<int> idx(n);
         iota(idx.begin(), idx.end(), 0);
 
         sort(idx.begin(), idx.end(),
-                [&](int i, int j) {
-                    return efficiency[i] > efficiency[j];
-                });
+             [&](int i, int j) { return efficiency[i] > efficiency[j]; });
 
         priority_queue<int, vector<int>, greater<int>> pq;
-        pq.push(speed[idx[0]]);
+        long long sum_speeds = 0;
+        long long best = 0;
 
-        long long max_performance = speed[idx[0]] * (long long)efficiency[idx[0]];
-        long long sum_speeds = speed[idx[0]];
+        for (int i = 0; i < n; ++i) {
+            const int cur = idx[i];
+            const int sp = speed[cur];
+            const int eff = efficiency[cur];
 
-        for(int i = 1; i < n; i++){
-            int cur_idx = idx[i];
+            if (i == 0) {
+                pq.push(sp);
+                sum_speeds = sp;
+                best = 1LL * sp * eff;
+                continue;
+            }
 
-            if(i < k || pq.top() <= speed[cur_idx]){
-                if(i >= k){
+            if (i < k || pq.top() <= sp) {
+                if (i >= k) {
                     sum_speeds -= pq.top();
                     pq.pop();
                 }
+                sum_speeds += sp;
+                pq.push(sp);
 
-                sum_speeds += speed[cur_idx];
-                pq.push(speed[cur_idx]);
-                max_performance = max(max_performance, sum_speeds * (long long)efficiency[cur_idx]);
+                const long long perf = sum_speeds * 1LL * eff;
+                if (perf > best) best = perf;
             }
         }
 
-        return (int)(max_performance % MOD);
+        return (int)(best % MOD);
     }
 };
