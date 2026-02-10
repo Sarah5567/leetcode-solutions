eb396a449f479b8010fd0efc11c4608e3faebec6
Sarah Gershuni <Sarah556726@gmail.com>
2026-02-10 22:34:11 +0200

    Time: 1176 ms (70.96%), Space: 19.7 MB (35.27%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.py
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.py
@@ -2,17 +2,18 @@ class Solution:
     def longestBalanced(self, nums: List[int]) -> int:
         n = len(nums)
         longest = 0
+        seen = set()
 
         for l in range(n):
-            distinct_nums = set()
             balance = 0
 
             for r in range(l, n):
-                if nums[r] not in distinct_nums:
-                    distinct_nums.add(nums[r])
+                if nums[r] not in seen:
+                    seen.add(nums[r])
                     balance += (1 if nums[r] % 2 else -1)
                 if balance == 0:
                     longest = max(longest, r - l + 1)
+            seen.clear()
             
         return longest
                       
