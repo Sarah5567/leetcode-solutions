05424eab8af9c48fd951e22d070831a24c310c17
Sarah Gershuni <Sarah556726@gmail.com>
2026-02-11 16:05:08 +0200

    Time: 32 ms (94.29%), Space: 19.3 MB (53.89%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -2,14 +2,22 @@ class Solution:
     def nthUglyNumber(self, n: int) -> int:
         dp = [0] * n
         dp[0] = 1
-        
-        pointers = [0, 0, 0]
-        fectors = [2, 3, 5]
 
-        for currIdx in range(1, n):
-            dp[currIdx] = min(dp[i] * f for i, f in zip(pointers, fectors))
-            for i in range(3):
-                if dp[pointers[i]] * fectors[i] == dp[currIdx]:
-                    pointers[i] += 1
+        p2 = p3 = p5 = 0
+        next2, next3, next5 = 2, 3, 5
 
-        return dp[n - 1]
+        for i in range(1, n):
+            x = min(next2, next3, next5)
+            dp[i] = x
+
+            if x == next2:
+                p2 += 1
+                next2 = dp[p2] * 2
+            if x == next3:
+                p3 += 1
+                next3 = dp[p3] * 3
+            if x == next5:
+                p5 += 1
+                next5 = dp[p5] * 5
+
+        return dp[-1]
