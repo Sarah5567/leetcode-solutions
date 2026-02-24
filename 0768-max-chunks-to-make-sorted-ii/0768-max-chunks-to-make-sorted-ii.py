a80c9ff206df2c22e28e3ebc3ef3aa6f32f0c803
Sarah Gershuni <Sarah556726@gmail.com>
2026-02-24 13:13:21 +0200

    Time: 20 ms (6.69%), Space: 19.5 MB (64.79%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.py
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
         sorted_arr = sorted(arr)
-        balance = defaultdict(int)
+        balance = Counter()
         chunks = 0
 
         for a, b in zip(arr, sorted_arr):
             balance[a] -= 1
             balance[b] += 1
 
-            if balance[a] == 0:
-                del balance[a]
-            if balance[b] == 0:
-                del balance[b]
+            for key in (a, b):
+                if balance[key] == 0:
+                    del balance[key]
 
             if not balance:
                 chunks += 1
