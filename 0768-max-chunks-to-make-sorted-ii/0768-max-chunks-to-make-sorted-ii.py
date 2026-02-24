1b2c3125d7ea5ac51a333b80fd6603a19ae14be7
Sarah Gershuni <Sarah556726@gmail.com>
2026-02-24 13:07:45 +0200

    Time: 7 ms (36.97%), Space: 19.6 MB (15.49%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.py
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.py
@@ -2,11 +2,9 @@ class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
         sorted_arr = sorted(arr)
         seen = defaultdict(int)
-        length = 0
         chunks = 0
 
         for val, sorted_val in zip(arr, sorted_arr):
-            length += 1
 
             seen[sorted_val] += 1
             if not seen[sorted_val]:
