253d2aa9417f8a0c7714fa66d4a1ebec1fcf029f
Sarah Gershuni <Sarah556726@gmail.com>
2026-02-24 13:11:31 +0200

    Time: 11 ms (17.96%), Space: 19.7 MB (8.45%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.py
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.py
@@ -3,18 +3,23 @@ class Solution:
         sorted_arr = sorted(arr)
         seen = defaultdict(int)
         chunks = 0
+        nonzero = 0
 
         for val, sorted_val in zip(arr, sorted_arr):
 
             seen[sorted_val] += 1
-            if not seen[sorted_val]:
-                del seen[sorted_val]
+            if seen[sorted_val] == 1:
+                nonzero += 1
+            if seen[sorted_val] == 0:
+                nonzero -= 1
 
             seen[val] -= 1
-            if not seen[val]:
-                del seen[val]
+            if seen[val] == -1:
+                nonzero += 1
+            if seen[val] == 0:
+                nonzero -= 1
 
-            if not seen:
+            if not nonzero:
                 chunks += 1
 
         return chunks
