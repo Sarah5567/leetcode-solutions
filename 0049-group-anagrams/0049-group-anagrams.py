9c98a150e1d59ab46f97a8092fdef57b06f8af20
Sarah Gershuni <Sarah556726@gmail.com>
2026-02-25 16:05:41 +0200

    Time: 45 ms (5.04%), Space: 21.9 MB (80.14%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,24 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+
+        def sort_string_linear(s: str) -> str:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            
+            result = []
+            for i in range(26):
+                result.append(chr(i + ord('a')) * count[i])
+        
+            return ''.join(result)
+
+        for s in strs:
+            sorted_s = sort_string_linear(s)
+            if sorted_s in anagrams:
+                anagrams[sorted_s].append(s)
+            else:
+                anagrams[sorted_s] = [s]
+
+        return list(anagrams.values())
+
