de8a1765d1779b587533f9228baf5a6901c41c46
Sarah Gershuni <Sarah556726@gmail.com>
2026-03-08 13:28:48 +0200

    Time: 39 ms (93.24%), Space: 23.1 MB (78.26%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.py
@@ -2,17 +2,15 @@ class Solution:
     def minKBitFlips(self, nums: List[int], k: int) -> int:
         n = len(nums)
 
-        flipped = 0          # parity of active flips
-        ans = 0              # number of flips
-        is_flipped = [0] * n # marks where a flip started
+        flipped = 0
+        ans = 0
+        is_flipped = [0] * n
 
         for i in range(n):
 
-            # remove flip effect when leaving the window
             if i >= k:
                 flipped ^= is_flipped[i - k]
 
-            # check current bit after flips
             if nums[i] ^ flipped == 0:
 
                 if i + k > n:
