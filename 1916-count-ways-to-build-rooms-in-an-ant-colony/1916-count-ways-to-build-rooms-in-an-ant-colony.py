6fbd84dbe7165e2c89761bec7d87e95fda6e316b
Sarah Gershuni <Sarah556726@gmail.com>
2026-03-08 12:58:58 +0200

    Time: 1386 ms (69.08%), Space: 120.8 MB (17.1%) - LeetHub

--- 1916-count-ways-to-build-rooms-in-an-ant-colony/1916-count-ways-to-build-rooms-in-an-ant-colony.py
+++ 1916-count-ways-to-build-rooms-in-an-ant-colony/1916-count-ways-to-build-rooms-in-an-ant-colony.py
@@ -1,15 +1,39 @@
 class Solution:
     def waysToBuildRooms(self, prevRoom):
+        MOD = 10**9 + 7
+        n = len(prevRoom)
+
         g = defaultdict(list)
-        for i in range(1, len(prevRoom)):
+        for i in range(1, n):
             g[prevRoom[i]].append(i)
 
+        fact = [1] * (n + 1)
+        inv_fact = [1] * (n + 1)
+
+        for i in range(1, n + 1):
+            fact[i] = fact[i - 1] * i % MOD
+
+        inv_fact[n] = pow(fact[n], MOD - 2, MOD)
+        for i in range(n, 0, -1):
+            inv_fact[i - 1] = inv_fact[i] * i % MOD
+
+        # fast combination
+        def comb(n, k):
+            return fact[n] * inv_fact[k] % MOD * inv_fact[n - k] % MOD
+
         def dfs(u):
-            size, ways = 1, 1
+            size = 1
+            ways = 1
+
             for v in g[u]:
                 s, w = dfs(v)
-                ways = ways * w * comb(size + s - 1, s)
+
+                # combine permutations
+                ways = ways * w % MOD
+                ways = ways * comb(size + s - 1, s) % MOD
+
                 size += s
+
             return size, ways
 
-        return dfs(0)[1] % (10**9 + 7)
+        return dfs(0)[1]
