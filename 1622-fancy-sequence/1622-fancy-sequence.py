f196db8056a40efa4be7af1e7f19d5a35788169c
Sarah Gershuni <Sarah556726@gmail.com>
2026-03-15 12:51:28 +0200

    Time: 264 ms (22.58%), Space: 54.4 MB (88.71%) - LeetHub

--- 1622-fancy-sequence/1622-fancy-sequence.py
+++ 1622-fancy-sequence/1622-fancy-sequence.py
@@ -3,20 +3,23 @@ class Fancy:
 
     def __init__(self):
         self.global_mul = 1
+        self.global_mul_inv = 1
         self.global_add = 0
         self.base_values = []
 
     def append(self, val: int) -> None:
-        inv = pow(self.global_mul, self.MOD - 2, self.MOD)
-        new_val = (val - self.global_add) * inv % self.MOD
+        new_val = (val - self.global_add) * self.global_mul_inv % self.MOD
         self.base_values.append(new_val)
 
     def addAll(self, inc: int) -> None:
         self.global_add = (self.global_add + inc) % self.MOD
 
     def multAll(self, m: int) -> None:
-        self.global_mul = (self.global_mul * m) % self.MOD
-        self.global_add = (self.global_add * m) % self.MOD
+        self.global_mul = self.global_mul * m % self.MOD
+        self.global_add = self.global_add * m % self.MOD
+
+        inv_m = pow(m, self.MOD - 2, self.MOD)
+        self.global_mul_inv = self.global_mul_inv * inv_m % self.MOD
 
     def getIndex(self, idx: int) -> int:
         if idx >= len(self.base_values):
