42c8f127ca443e4c0c9e175f1e2fdbbc7f5b587f
Sarah Gershuni <Sarah556726@gmail.com>
2026-03-16 14:33:14 +0200

    Time: 0 ms (100%), Space: 19.2 MB (83.86%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        trailing_zeroes = 0
+        five_factor = 5
+        
+        while five_factor <= n:
+            trailing_zeroes += n // five_factor
+            five_factor *= 5
+        
+        return trailing_zeroes
