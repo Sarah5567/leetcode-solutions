268b5af4c5a28a1cad295bd348475c7b6ecc8cc1
Sarah Gershuni <Sarah556726@gmail.com>
2026-03-17 13:44:10 +0200

    Time: 112 ms (53.76%), Space: 67.7 MB (89.38%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.py
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.py
@@ -1,32 +1,32 @@
 class RandomizedCollection:
 
     def __init__(self):
-        self.arr = [] 
+        self.arr = []
         self.pos = defaultdict(set)
-        
+
     def insert(self, val: int) -> bool:
-        exists = val in self.pos and len(self.pos[val]) > 0
-        
-        self.arr.append((val, len(self.pos[val])))
+        exists = len(self.pos[val]) > 0
+
+        self.arr.append(val)
         self.pos[val].add(len(self.arr) - 1)
-        
+
         return not exists
 
     def remove(self, val: int) -> bool:
         if not self.pos[val]:
             return False
-        
+
         remove_idx = self.pos[val].pop()
-        last_val, last_idx_in_hash = self.arr[-1]
-        
-        self.arr[remove_idx] = (last_val, last_idx_in_hash)
-        
+        last_val = self.arr[-1]
+
+        self.arr[remove_idx] = last_val
+
         self.pos[last_val].add(remove_idx)
-        self.pos[last_val].remove(len(self.arr) - 1)
-        
+        self.pos[last_val].discard(len(self.arr) - 1)
+
         self.arr.pop()
-        
+
         return True
 
     def getRandom(self) -> int:
-        return random.choice(self.arr)[0]
+        return random.choice(self.arr)
