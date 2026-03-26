6cec6405ff486cc2404f0ca0c71160ae9c8bab32
Sarah Gershuni <Sarah556726@gmail.com>
2026-03-26 13:40:34 +0200

    Time: 1247 ms (38.89%), Space: 53.6 MB (36.11%) - LeetHub

--- 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.py
+++ 3548-equal-sum-grid-partition-ii/3548-equal-sum-grid-partition-ii.py
@@ -1,6 +1,11 @@
 class Solution:
     def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        m, n = len(grid), len(grid[0])
         
+        flat_grid = list(itertools.chain.from_iterable(grid))
+        total_sum = sum(flat_grid)
+        base_counter = Counter(flat_grid)
+
         def check_partition(part_sum, other_sum, current_counter, rows, cols, ends):
             if part_sum == other_sum:
                 return True
@@ -12,40 +17,52 @@ class Solution:
                 return True
             return diff in ends
 
-        def check_orientation(matrix) -> bool:
-            r, c = len(matrix), len(matrix[0])
-            
-            flat_matrix = list(itertools.chain.from_iterable(matrix))
-            top_counter = Counter()
-            bottom_counter = Counter(flat_matrix)
-            
-            top_sum = 0
-            bottom_sum = sum(flat_matrix)
-            
-            for i in range(r - 1):
-                row = matrix[i]
-                row_sum = sum(row)
-                
-                for val in row:
-                    top_counter[val] += 1
-                    bottom_counter[val] -= 1
-                
-                top_sum += row_sum
-                bottom_sum -= row_sum
-                
-                top_ends = {matrix[0][0], matrix[0][c-1], matrix[i][0], matrix[i][c-1]}
-                if check_partition(top_sum, bottom_sum, top_counter, i + 1, c, top_ends):
-                    return True
-                
-                bottom_ends = {matrix[i+1][0], matrix[i+1][c-1], matrix[r-1][0], matrix[r-1][c-1]}
-                if check_partition(bottom_sum, top_sum, bottom_counter, r - i - 1, c, bottom_ends):
-                    return True
-                    
-            return False
-
-        if check_orientation(grid):
-            return True
-            
-        transposed_grid = [list(col) for col in zip(*grid)]
+        top_counter = Counter()
+        bottom_counter = base_counter.copy()
+        top_sum = 0
+        bottom_sum = total_sum
         
-        return check_orientation(transposed_grid)
+        for i in range(m - 1):
+            row = grid[i]
+            row_sum = sum(row)
+            
+            for val in row:
+                top_counter[val] += 1
+                bottom_counter[val] -= 1
+            
+            top_sum += row_sum
+            bottom_sum -= row_sum
+            
+            top_ends = {grid[0][0], grid[0][n-1], grid[i][0], grid[i][n-1]}
+            if check_partition(top_sum, bottom_sum, top_counter, i + 1, n, top_ends):
+                return True
+            
+            bottom_ends = {grid[i+1][0], grid[i+1][n-1], grid[m-1][0], grid[m-1][n-1]}
+            if check_partition(bottom_sum, top_sum, bottom_counter, m - i - 1, n, bottom_ends):
+                return True
+
+        left_counter = Counter()
+        right_counter = base_counter.copy()
+        left_sum = 0
+        right_sum = total_sum
+        
+        for j in range(n - 1):
+            col_sum = 0
+            for i in range(m):
+                val = grid[i][j]
+                col_sum += val
+                left_counter[val] += 1
+                right_counter[val] -= 1
+            
+            left_sum += col_sum
+            right_sum -= col_sum
+            
+            left_ends = {grid[0][0], grid[m-1][0], grid[0][j], grid[m-1][j]}
+            if check_partition(left_sum, right_sum, left_counter, m, j + 1, left_ends):
+                return True
+            
+            right_ends = {grid[0][j+1], grid[m-1][j+1], grid[0][n-1], grid[m-1][n-1]}
+            if check_partition(right_sum, left_sum, right_counter, m, n - j - 1, right_ends):
+                return True
+
+        return False
