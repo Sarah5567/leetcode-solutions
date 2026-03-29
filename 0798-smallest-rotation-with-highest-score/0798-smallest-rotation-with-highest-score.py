3fdacf72b99db31875e05271a454383fc195da36
Sarah Gershuni <Sarah556726@gmail.com>
2026-03-29 04:54:45 +0300

    Time: 149 ms (23.36%), Space: 30.3 MB (80.37%) - LeetHub

---
+++ 0798-smallest-rotation-with-highest-score/0798-smallest-rotation-with-highest-score.py
@@ -0,0 +1,29 @@
+class Solution:
+    def bestRotation(self, nums: List[int]) -> int:
+        n = len(nums)
+        change = [0] * n
+
+        score = 0
+        for i, val in enumerate(nums):
+            if val <= i:
+                score += 1
+            
+            low = (i + 1) % n
+            high = (i - val + n + 1) % n
+            
+            change[low] += 1
+            change[high] -= 1
+            
+            if low >= high:
+                change[0] += 1
+
+        max_score = score
+        best_k = 0
+
+        for k in range(1, n):
+            score += change[k]
+            if score > max_score:
+                max_score = score
+                best_k = k
+
+        return best_k
