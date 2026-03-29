6cf9a6b1447125181e65c83b568f14e16c535b03
Sarah Gershuni <Sarah556726@gmail.com>
2026-03-29 04:56:09 +0300

    Time: 143 ms (32.71%), Space: 30.2 MB (86.92%) - LeetHub

--- 0798-smallest-rotation-with-highest-score/0798-smallest-rotation-with-highest-score.py
+++ 0798-smallest-rotation-with-highest-score/0798-smallest-rotation-with-highest-score.py
@@ -4,23 +4,25 @@ class Solution:
         change = [0] * n
 
         score = 0
-        for i in range(n):
-            val = nums[i]
+        
+        # initial score + build change array
+        for i, val in enumerate(nums):
             if val <= i:
                 score += 1
-            
+
             low = (i + 1) % n
-            high = (i - val + n + 1) % n
-            
+            high = (i - val + 1) % n
+
             change[low] += 1
             change[high] -= 1
-            
+
             if low >= high:
                 change[0] += 1
 
         max_score = score
         best_k = 0
 
+        # sweep line
         for k in range(1, n):
             score += change[k]
             if score > max_score:
