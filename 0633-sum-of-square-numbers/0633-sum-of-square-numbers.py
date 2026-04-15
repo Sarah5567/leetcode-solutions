97018d5808c551ad9aae6c1afc50bb1ca060679b
Sarah Gershuni <Sarah556726@gmail.com>
2026-04-15 12:41:24 +0300

    Time: 90 ms (28.24%), Space: 19.3 MB (67.23%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,11 +1,13 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        base1 = 1
-        max_base1 = int((c // 2) ** 0.5)
+        max_base1 = int(c ** 0.5)
 
         for base1 in range(max_base1 + 1):
-            base2 = (c - base1 ** 2) ** 0.5
-            if base2 == int(base2):
+            base1_sq = base1 * base1
+            base2_sq = c - base1_sq
+            base2 = int(base2_sq ** 0.5)
+
+            if base2 * base2 == base2_sq:
                 return True
 
         return False
