de1984cacc9ea85fe48bc4ab64ae207cbeb7b705
Sarah Gershuni <Sarah556726@gmail.com>
2026-04-21 02:17:33 +0300

    Time: 111 ms (35.22%), Space: 21.1 MB (82.06%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -1,17 +1,19 @@
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
+        stone_to_index = {stone: i for i, stone in enumerate(stones)}
         n = len(stones)
 
         dp = [set() for _ in range(n)]
         dp[0].add(0)
 
-        for i in range(1, n):
-            for j in range(i - 1, -1, -1):
-                k = stones[i] - stones[j]
-                if k > j + 1:
-                    break
+        for i in range(n):
+            for k in dp[i]:
+                for jump in (k - 1, k, k + 1):
+                    if jump <= 0:
+                        continue
 
-                if k in dp[j] or k - 1 in dp[j] or k + 1 in dp[j]:
-                    dp[i].add(k)
+                    next_pos = stones[i] + jump
+                    if next_pos in stone_to_index:
+                        dp[stone_to_index[next_pos]].add(jump)
 
         return bool(dp[-1])
