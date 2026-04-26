2482e9cde1f819fd7d4dc2da03a96f609d11ad5d
Sarah Gershuni <Sarah556726@gmail.com>
2026-04-26 17:30:55 +0300

    Time: 59 ms (49.27%), Space: 20.6 MB (51.24%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -1,17 +1,18 @@
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         q = deque()
-
         q.append(root)
-        q.append(None)
+        prev = 0
 
-        while len(q) > 1:
-            node = q.popleft()
-            if node:
-                node.next = q[0]
+        while q[0]:
+            size = max(1, prev * 2)
+            for _ in range(size):
+                node = q.popleft()
                 q.append(node.left)
                 q.append(node.right)
-            elif q[-1]:
-                    q.append(None)
+                node.next = q[0]
+
+            node.next = None
+            prev = size
 
         return root
