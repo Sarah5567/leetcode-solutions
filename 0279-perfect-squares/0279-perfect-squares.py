675a9fd740473fbeb228dc8362d8a02bed8c81ae
Sarah Gershuni <Sarah556726@gmail.com>
2026-04-26 18:44:59 +0300

    Time: 2127 ms (24.29%), Space: 19.2 MB (78.63%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -4,10 +4,12 @@ class Solution:
         sums[0] = 0
         n_square = int(n ** 0.5)
 
-        for cur_sum in range(1, n + 1):
+        for cur_sum in range(n):
             for root in range(1, n_square + 1):
                 square_number = root * root
-                if square_number <= cur_sum:
-                    sums[cur_sum] = min(sums[cur_sum], sums[cur_sum - square_number] + 1)
+                if square_number + cur_sum <= n:
+                    sums[square_number + cur_sum] = min(sums[cur_sum] + 1, sums[square_number + cur_sum])
+                else:
+                    break
 
         return sums[n]
