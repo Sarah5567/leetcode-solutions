8491c265a1eaa8ef714f15282e110842af765181
Sarah Gershuni <Sarah556726@gmail.com>
2026-04-26 15:36:04 +0300

    Time: 429 ms (47.45%), Space: 142.4 MB (13.29%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
@@ -3,16 +3,13 @@ class Solution:
         m, n = len(grid), len(grid[0])
         visited = [[False] * n for _ in range(m)]
 
-        def is_valid_cell(r: int, c: int) -> bool:
-            return r < m and r >= 0 and c < n and c >= 0
-
         def dfs(r: int, c: int, source) -> bool:
             visited[r][c] = True
 
             neighbors = [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]]
 
             for r_neighbor, c_neighbor in neighbors:
-                if not is_valid_cell(r_neighbor, c_neighbor):
+                if r_neighbor == m or r_neighbor < 0 or c_neighbor == n or c_neighbor < 0:
                     continue
 
                 if not grid[r_neighbor][c_neighbor] == grid[r][c]:
