0219a754dc2f835f60c33f630ad8c2f67e79746d
Sarah Gershuni <Sarah556726@gmail.com>
2026-04-26 18:41:44 +0300

    Time: 1900 ms (30.24%), Space: 19.3 MB (78.63%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        sums = [float('inf')] * (n + 1)
+        sums[0] = 0
+
+        for cur_sum in range(1, n + 1):
+            for root in range(1, int(n ** 0.5) + 1):
+                square_number = root * root
+                if square_number <= cur_sum:
+                    sums[cur_sum] = min(sums[cur_sum], sums[cur_sum - square_number] + 1)
+
+        return sums[n]
