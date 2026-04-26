696b2f89722dd27e48c2068076ea81d633474b94
Sarah Gershuni <Sarah556726@gmail.com>
2026-04-26 15:30:45 +0300

    Time: 499 ms (27.24%), Space: 141.9 MB (13.81%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
@@ -0,0 +1,26 @@
+class Solution:
+    def containsCycle(self, grid: List[List[str]]) -> bool:
+        m, n = len(grid), len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+
+        def is_valid_cell(r: int, c: int) -> bool:
+            return r < m and r >= 0 and c < n and c >= 0
+
+        def dfs(r: int, c: int, source) -> bool:
+            visited[r][c] = True
+
+            neighbors = [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]]
+
+            for r_neighbor, c_neighbor in neighbors:
+                if is_valid_cell(r_neighbor, c_neighbor) and grid[r_neighbor][c_neighbor] == grid[r][c] and (r_neighbor, c_neighbor) != source:
+                        if visited[r_neighbor][c_neighbor] or dfs(r_neighbor, c_neighbor, (r, c)):
+                            return True
+                        
+            return False
+
+        for r in range(m):
+            for c in range(n):
+                if not visited[r][c] and dfs(r, c, (-1, -1)):
+                    return True
+
+        return False
