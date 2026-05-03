6672c4fef5bd7334dbc78e11acb2edddfb25faff
Sarah Gershuni <Sarah556726@gmail.com>
2026-05-03 15:23:40 +0300

    Time: 63 ms (29.07%), Space: 20.4 MB (42.48%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,13 +1,17 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        s = s.replace(' ', '')
         stack = [1]
         sign = 1
         answer = 0
         index = 0
+        n = len(s)
 
-        while index < len(s):
+        while index < n:
             char = s[index]
+
+            if char == ' ':
+                index += 1
+                continue
 
             if char == '+':
                 sign = 1
@@ -15,19 +19,19 @@ class Solution:
                 sign = -1
             elif char == '(':
                 stack.append(sign * stack[-1])
-                sign = 1 
+                sign = 1
             elif char == ')':
                 stack.pop()
             elif char.isdigit():
-                last_index = index
-                while last_index + 1 < len(s) and s[last_index + 1].isdigit():
-                    last_index += 1
-                
-                current_num = int(s[index:last_index + 1])
-                answer += current_num * sign * stack[-1]
-                
-                index = last_index
+                num = 0
+                while index < n and s[index].isdigit():
+                    num = num * 10 + (ord(s[index]) - ord('0'))
+                    index += 1
+
+                answer += num * sign * stack[-1]
+                index -= 1
 
             index += 1
 
         return answer
+        
