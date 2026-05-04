3e8f6d5fe5c3b6c57a1b59fc0154850e2bfa7ce6
Sarah Gershuni <Sarah556726@gmail.com>
2026-05-04 20:47:59 +0300

    Time: 109 ms (32.15%), Space: 33.2 MB (27.4%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -3,8 +3,12 @@ class Solution:
         m, n = len(matrix), len(matrix[0])
         dp = [[int(cell) for cell in row] for row in matrix]
 
+        largest = 1 if any(val for val in dp[0]) or any(dp[i][0] for i in range(m)) else 0
+
         for i in range(1, m):
             for j in range(1, n):
-                dp[i][j] += (min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])) * dp[i][j]
+                if dp[i][j]:
+                    dp[i][j] += min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+                    largest = max(largest, dp[i][j])
 
         return max(max(row) for row in dp) ** 2
