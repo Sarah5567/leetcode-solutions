46278cd5397074caab7f64b1140c2569e81e6f31
Sarah Gershuni <Sarah556726@gmail.com>
2026-05-05 17:55:32 +0300

    Time: 3 ms (39.02%), Space: 19.1 MB (98.99%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,26 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        entries = ['']
+        for ch in path:
+            if ch == '/':
+                if entries and entries[-1] == '.':
+                    entries.pop()
+                elif entries and entries[-1] == '..':
+                    entries.pop()
+                    if entries:
+                        entries.pop()
+
+                if not entries or entries[-1]:
+                    entries.append('')
+
+            else:
+                entries[-1] += ch
+
+        if entries and entries[-1] in ['.', '']:
+            entries.pop()
+        elif entries and entries[-1] == '..':
+            entries.pop()
+            if entries:
+                entries.pop()
+
+        return '/' + '/'.join(entries)
