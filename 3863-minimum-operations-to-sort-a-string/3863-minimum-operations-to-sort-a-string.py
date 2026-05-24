5d5572c05a82fb4003d3b237cf4cc9965812207d
Sarah Gershuni <Sarah556726@gmail.com>
2026-05-24 21:04:55 +0300

    Time: 415 ms (25.26%), Space: 20.1 MB (70.62%) - LeetHub

--- 3863-minimum-operations-to-sort-a-string/3863-minimum-operations-to-sort-a-string.py
+++ 3863-minimum-operations-to-sort-a-string/3863-minimum-operations-to-sort-a-string.py
@@ -5,11 +5,23 @@ class Solution:
 
         max_val = 'a'
         min_val = 'z'
+        count_min = 0
+        count_max = 0
         is_sorted = True
 
         for i in range(len(s)):
-            max_val = max(max_val, s[i])
-            min_val = min(min_val, s[i])
+            if s[i] > max_val:
+                max_val = s[i]
+                count_max = 1
+            elif s[i] == max_val:
+                count_max += 1
+
+            if s[i] < min_val:
+                min_val = s[i]
+                count_min = 1
+            elif s[i] == min_val:
+                count_min += 1
+
             if i > 0 and s[i] < s[i - 1]:
                 is_sorted = False
 
@@ -17,7 +29,7 @@ class Solution:
             return 0
         elif s[0] == min_val or s[-1] == max_val:
             return 1
-        elif s[0] != max_val or s[-1] != min_val or s.count(max_val) > 1 or s.count(min_val) > 1:
+        elif s[0] != max_val or s[-1] != min_val or count_max > 1 or count_min > 1:
             return 2
         else:
             return 3
