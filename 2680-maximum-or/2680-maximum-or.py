70bafbc01f70cabe91db02e3f9dd2e603e75b5fe
Sarah Gershuni <Sarah556726@gmail.com>
2026-05-24 20:29:30 +0300

    Time: 121 ms (80.77%), Space: 33.2 MB (83.33%) - LeetHub

---
+++ 2680-maximum-or/2680-maximum-or.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumOr(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        others_or = [0] * n
+        prefix = 0
+        suffix = 0
+
+        for i in range(n):
+            others_or[i] |= prefix
+            others_or[n - i - 1] |= suffix
+
+            prefix |= nums[i]
+            suffix |= nums[n - i - 1]
+
+        best = 0
+        for num, or_operation in zip(nums, others_or):
+            best = max(best, or_operation | num << k)
+        
+        return best
+
+
