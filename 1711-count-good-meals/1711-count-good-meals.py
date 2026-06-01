d36ca6729a09e57e7c3a4758d0a1ad694e410d96
Sarah Gershuni <Sarah556726@gmail.com>
2026-06-01 15:51:27 +0300

    Time: 163 ms (97.09%), Space: 28.7 MB (26.21%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -0,0 +1,26 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def countPairs(self, deliciousness: List[int]) -> int:
+        MOD = 1000000007
+        seen = defaultdict(int)
+        need = defaultdict(int)
+
+        meals = 0
+
+        for d in deliciousness:
+            if d == 0:
+                meals = (meals + need[0]) % MOD
+                seen[0] += 1
+            else:
+                closest_pow = 1 << (d - 1).bit_length()
+                meals = (meals + seen[closest_pow - d] + need[d]) % MOD
+                
+                seen[d] += 1
+                need[closest_pow - d] += 1
+                if d == closest_pow:
+                    need[d] += 1
+
+        return meals
+        
