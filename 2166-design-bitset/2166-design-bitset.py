a7487e853e057846581e5dccdad4321b8af3c038
Sarah Gershuni <Sarah556726@gmail.com>
2026-06-02 21:35:08 +0300

    Time: 197 ms (43.75%), Space: 51 MB (85.71%) - LeetHub

--- 2166-design-bitset/2166-design-bitset.py
+++ 2166-design-bitset/2166-design-bitset.py
@@ -30,13 +30,3 @@ class Bitset:
 
     def toString(self) -> str:
         return ''.join('1' if bit != self.is_fliped else '0' for bit in self.bits)
-
-# Your Bitset object will be instantiated and called as such:
-# obj = Bitset(size)
-# obj.fix(idx)
-# obj.unfix(idx)
-# obj.flip()
-# param_4 = obj.all()
-# param_5 = obj.one()
-# param_6 = obj.count()
-# param_7 = obj.toString()
