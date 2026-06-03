9a6b12331523e2ba1bffcc1f28c16e842fd85b12
Sarah Gershuni <Sarah556726@gmail.com>
2026-06-03 19:48:48 +0300

    Time: 2 ms (17.39%), Space: 19.3 MB (85.22%) - LeetHub

---
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.py
@@ -0,0 +1,20 @@
+class Solution:
+    def makeIntegerBeautiful(self, n: int, target: int) -> int:
+        x = 0
+        power = 1
+        sum_digits = sum(int(d) for d in str(n))
+
+        while sum_digits > target:
+            digit = n % 10
+
+            if digit:
+                addition = 10 - digit
+                n += addition
+                x += addition * power
+
+            power *= 10
+            n //= 10
+            sum_digits = sum(int(d) for d in str(n))
+
+
+        return x
