c9249c8fbf7d6505ee3044ca71af339497aa61fd
Sarah Gershuni <Sarah556726@gmail.com>
2026-06-03 19:00:32 +0300

    Time: 28 ms (97.58%), Space: 19.3 MB (95.97%) - LeetHub

--- 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.py
+++ 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.py
@@ -3,7 +3,6 @@ class Solution:
         n = len(nums)
         prev_index = {}
         dp = [0] * n
-        max_length = 0
 
         for _ in range(k + 1):
             next_dp = [0] * n
@@ -14,10 +13,9 @@ class Solution:
                     next_dp[i] = max(next_dp[i], next_dp[prev_index[num]] + 1)
 
                 prev_index[num] = i
-                max_length = max(max_length, next_dp[i])
                 prev_max = max(prev_max, dp[i])
 
             dp = next_dp
             prev_index = {}
 
-        return max_length
+        return max(dp)
