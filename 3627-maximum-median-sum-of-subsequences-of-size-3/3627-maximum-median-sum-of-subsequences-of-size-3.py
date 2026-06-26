898ac223e2de71bb2f00c6dcfe8e0d3de7cdd072
Sarah Gershuni <Sarah556726@gmail.com>
2026-06-26 03:09:58 +0300

    Time: 61 ms (92.73%), Space: 73.8 MB (56.36%) - LeetHub

--- 3627-maximum-median-sum-of-subsequences-of-size-3/3627-maximum-median-sum-of-subsequences-of-size-3.py
+++ 3627-maximum-median-sum-of-subsequences-of-size-3/3627-maximum-median-sum-of-subsequences-of-size-3.py
@@ -1,6 +1,5 @@
 class Solution:
     def maximumMedianSum(self, nums: List[int]) -> int:
         n = len(nums)
-        nums.sort()
-        
-        return sum(nums[i] for i in range(int(n / 3), n, 2))
+        nums.sort() 
+        return sum(nums[i] for i in range(n // 3, n, 2))
