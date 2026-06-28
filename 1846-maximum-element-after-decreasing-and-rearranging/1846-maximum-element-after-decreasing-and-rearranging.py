899c0ea6efce71ebe4aecf7f2e4803804a27adc2
Sarah Gershuni <Sarah556726@gmail.com>
2026-06-28 14:50:31 +0300

    Time: 28 ms (56.03%), Space: 36.5 MB (5.67%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -3,7 +3,8 @@ class Solution:
         counts = Counter(arr)
         next_val = 1
 
-        for val, count in sorted(counts.items()):
-            next_val = min(val + 1, next_val + count)
+        for val in sorted(counts):
+            next_val = min(val + 1, next_val + counts[val])
 
         return next_val - 1
+        
